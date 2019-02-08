bb87a50b7aaf6893bc08bfce170a7c53c7f9eb82
Valentina0016 <47438689+Valentina0016@users.noreply.github.com>
2019-02-08 13:27:44 -0500

    Update Exercise_08_05.java

--- Exercise_08/Exercise_08_05/Exercise_08_05.java
+++ Exercise_08/Exercise_08_05/Exercise_08_05.java
@@ -10,69 +10,63 @@
 * Write a test program that prompts the user to enter two 3 * 3 matrices and     *
 * displays their sum.                                                            *
 *********************************************************************************/
+// TODO Auto-generated method stub
 import java.util.Scanner;
-
-public class Exercise_08_05 {
-	/** Main method */
+	public class Exercise_08_05 {
 	public static void main(String[] args) {
 		// Initialize two 3 x 3 matrices
-		double[][] matrix1 = getmatrix(1);
-		double[][] matrix2 = getmatrix(2);
+		int rows = 3;
+		int cols = 3;
+		double[][] matrix1 = new double[rows][cols];
+		double[][] matrix2 = new double[rows][cols];
 
-		// Initialize matrix3 with the sum of matrix1 and matrix2
-		double[][] matrix3 = addMatrix(matrix1, matrix2);
-
-		// Display matrix1, matrix2 and matrix3
-		print(matrix1, matrix2, matrix3);
-	}
-
-	/** getmatrix */
-	public static double[][] getmatrix(int n) {
-		// Create a Scanner
 		Scanner input = new Scanner(System.in);
 
-		// Prompt the user to enter a 3 x 3 matrix
-		System.out.print("Enter matrix" + n + ": ");
-
-		double[][] m = new double[3][3];
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++)
-				m[i][j] = input.nextDouble();
-		}
-
-		return m;
-	}
-
-	/** addMatrix adds two matrices and returns the result */
-	public static double[][] addMatrix(double[][] a, double[][] b) {
-		double[][] c = new double[3][3];
-
-		for (int i = 0; i < c.length; i++) {
-			for (int j = 0; j < c[i].length; j++) {
-				c[i][j] = a[i][j] + b[i][j];
+		// Prompt the user to enter a 3-by-3 matrix
+		System.out.print("Enter matrix1: ");
+		for (int row = 0; row < matrix1.length; row++){
+			for (int col = 0; col < matrix1[row].length; col++) {
+				matrix1[row][col] = input.nextDouble();
 			}
 		}
-		return c;
-	}
-
-	/** print displays all the elements in row r */
-	public static void print(double[][] m, int r) {	
-		for (int j = 0; j < m[r].length; j++) {
-			System.out.print(m[r][j] + " ");
-		}	
-	}
-
-	/** print displays results */
-	public static void print(double[][] m1, double[][] m2, double[][] m3) {
-		System.out.println("The matrices are added as follows");
 		
-		for (int i = 0; i < 3; i++) {
-			print(m1, i);
-			System.out.print((i == 1 ? "  +  " : "     "));
-			print(m2, i);
-			System.out.print((i == 1 ? "  =  " : "     "));
-			print(m3, i);
-			System.out.println();
+		System.out.print("Enter matrix2: ");
+		for (int row = 0; row < matrix2.length; row++){
+			for (int col = 0; col < matrix2[row].length; col++) {
+				matrix2[row][col] = input.nextDouble();
+			}
 		}
+
+		double[][] result =  addMartix(matrix1, matrix2);
+
+		// add the matrices
+
+      //Print out
+		System.out.println("The matrices are added as follows");
+		System.out.println(matrix1[0][0] + " " +matrix1[0][1] +" "+
+				matrix1[0][2] + "       " +matrix2[0][0] + " " +matrix2[0][1] +" "+matrix2[0][2]
+						+"       " +result[0][0] + " " +result[0][1] +" "+result[0][2]);
+
+		System.out.println(matrix1[1][0] + " " +matrix1[1][1] +" "+
+				matrix1[1][2] + "   +   " +matrix2[1][0] + " " +matrix2[1][1] +" "+matrix2[1][2]
+						+"   =   " +result[1][0] + " " +result[1][1] +" "+result[1][2]);
+
+		System.out.println(matrix1[2][0] + " " +matrix1[2][1] +" "+
+				matrix1[2][2] + "       " +matrix2[2][0] + " " +matrix2[2][1] +" "+matrix2[2][2]
+						+"       " +result[2][0] + " " +result[2][1] +" "+result[2][2]);
 	}
+
+
+	public static double[][] addMartix(double[][] a, double[][] b){
+		double [][] sum = new double [3][3];
+		int row,column;
+		for(row = 0;row < a.length;row++){
+			for(column = 0;column < a[row].length;column++){
+				sum[row][column] = a[row][column]+b[row][column];
+			}
+		}
+		return sum;
+	}
+
 }
+
